9a16e8715d5ff1a3ab373170a7fd4887477bb684
root <root@shirtcity.com>
2017-04-06 16:52:06 +0200

    ProductBundle beta 1.1.1

--- src/Elcodi/Bundle/ProductBundle/Entity/Interfaces/ProductInterface.php
+++ src/Elcodi/Bundle/ProductBundle/Entity/Interfaces/ProductInterface.php
@@ -9,7 +9,14 @@ use Elcodi\Component\Core\Entity\Interfaces\EnabledInterface;
 use Elcodi\Component\Core\Entity\Interfaces\ETaggableInterface;
 use Elcodi\Component\Media\Entity\Interfaces\ImagesContainerWithPrincipalImageInterface;
 use Elcodi\Component\MetaData\Entity\Interfaces\MetaDataInterface;
+
 use Elcodi\Bundle\PriceBundle\Entity\Interfaces\PriceInterface;
+use Elcodi\Bundle\ProductBundle\Entity\Interfaces\ProductColorsInterface;
+use Elcodi\Bundle\ProductBundle\Entity\Interfaces\ProductSizesInterface;
+use Elcodi\Bundle\ProductBundle\Entity\Interfaces\ProductManufacturerInterface;
+use Elcodi\Bundle\ProductBundle\Entity\Interfaces\PrintSideInterface;
+use Elcodi\Bundle\ProductBundle\Entity\Interfaces\PrintMethodInterface;
+use Elcodi\Bundle\CategoryBundle\Entity\Interfaces\CategoryInterface;
 
 /**
  * Interface ProductInterface.
@@ -207,11 +214,93 @@ interface ProductInterface
 	 * 
 	 * @param ArrayCollection $printSides
 	 * @return $this
-	 */
-	
+	 */	
 	public function setPrintSides(ArrayCollection $printSides);
 	
+	/**
+     * Add printMethod
+     *
+     * @param PrintMethodInterface $printMethod
+     *
+     * @return Product
+     */
+    public function addPrintMethod(PrintMethodInterface $printMethod);
+
     /**
+     * Remove printMethod
+     *
+     * @param PrintMethodInterface $printMethod
+     */
+    public function removePrintMethod(PrintMethodInterface $printMethod);
+
+    /**
+     * Get printMethods
+     *
+     * @return Collection
+     */
+    public function getPrintMethods();
+	
+	/**
+     * Add section
+     *
+     * @param CategoryInterface $section
+     *
+     * @return Product
+     */
+    public function addSection(CategoryInterface $section);
+
+    /**
+     * Remove section
+     *
+     * @param CategoryInterface $section
+     */
+    public function removeSection(CategoryInterface $section);
+
+    /**
+     * Get sections
+     *
+     * @return Collection
+     */
+    public function getSections();
+
+    /**
+     * Add productCategory
+     *
+     * @param CategoryInterface $productCategory
+     *
+     * @return Product
+     */
+    public function addProductCategory(CategoryInterface $productCategory);
+
+    /**
+     * Remove productCategory
+     *
+     * @param CategoryInterface $productCategory
+     */
+    public function removeProductCategory(CategoryInterface $productCategory);
+
+    /**
+     * Get productCategories
+     *
+     * @return Collection
+     */
+    public function getProductCategories();
+	
+	/**
+	 * Return Product Sizes and Colors variants.
+	 * 
+	 * @return array
+	 */
+	public function getVariants();
+	
+	/**
+	 * Set Product Sizes and Colors variants.
+	 * 
+	 * @return $this
+	 */
+	public function setVariants();
+	
+	/**
      * To string method.
      *
      * @return string
